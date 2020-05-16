5b8c518d15495f56cd31a39278fee24fd35a3bb6
KeshavPrince <keshavprince30gmail.com>
2020-05-16 09:28:03 +0530

    done

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,17 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <div>
-      {/* <nav className="nav-wrapper">
-                <div className="container">
-                <a href = "/" className="brand-logo">HireSnapper</a>
-                <ul className = "right">
-                    <li><a href = "/">Home</a></li>
-                    <li><a href = "/">About</a></li>
-                    <li><a href = "/">Contact Us</a></li>
-                    <li><a href = "/authenticate/signup">Join Us</a></li>
-                </ul>
-                </div>
-            </nav> */}
+      
       <div class="topnav">
         <a class="active" href="#home">
           Home
