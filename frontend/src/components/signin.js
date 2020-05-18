180c78af3dcba07e5fedfc882065412956b6846b
KeshavPrince <keshavprince30gmail.com>
2020-05-18 14:14:21 +0530

    checking

--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -35,7 +35,7 @@ export default function Signin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('wolfies');
+    console.log('wolfie');
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
